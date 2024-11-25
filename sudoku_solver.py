bf4cbe23db1cd10dafbaf538830ad3bfcf256195
Brian Roepke <broepke@gmail.com>
2024-11-25 17:32:37 -0600

    Add basic logging

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -4,9 +4,18 @@ import cv2
 import pytesseract
 import tempfile
 import platform
-print("OpenCV Version:", cv2.__version__)
-print("Pytesseract Version:", pytesseract.get_tesseract_version())
-print("Python Version:", platform.python_version())
+import logging
+
+# Configure the logger
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+# Log the information
+logging.info("OpenCV Version: %s", cv2.__version__)
+logging.info("Pytesseract Version: %s", pytesseract.get_tesseract_version())
+logging.info("Python Version: %s", platform.python_version())
 
 
 def preprocess_image(image):
